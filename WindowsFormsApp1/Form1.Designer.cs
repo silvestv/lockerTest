fc715ccf7e3e734cc5a5dbcbd81ef4491c7e6161
silvestv <victor.silvestre0@gmail.com>
2020-01-18 01:44:42 +0100

    Locker winForm et insertion, update, restriction en BDD termineé manque plus que le service !

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -41,10 +41,35 @@ namespace WindowsFormsApp1
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.appBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.radioButtonUnlock = new System.Windows.Forms.RadioButton();
+            this.radioButtonLock = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.appBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // listBox1
@@ -71,34 +96,243 @@ namespace WindowsFormsApp1
             this.label1.Size = new System.Drawing.Size(162, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Le nom de l\'application a stopper";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(251, 83);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(451, 336);
-            this.dataGridView1.TabIndex = 3;
             // 
             // appBindingSource
             // 
             this.appBindingSource.CurrentChanged += new System.EventHandler(this.bindingSource1_CurrentChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(485, 305);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(48, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(485, 344);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(48, 20);
+            this.numericUpDown2.TabIndex = 4;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(584, 305);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(48, 20);
+            this.numericUpDown3.TabIndex = 5;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(584, 344);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(48, 20);
+            this.numericUpDown4.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(361, 307);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Temps d\'authorisation";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(376, 346);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Temps de blocage";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(539, 351);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "heures";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(539, 307);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "heures";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(638, 307);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "minutes";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(638, 351);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "minutes";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(503, 396);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Enregistrer la restriction";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(389, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(268, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(389, 93);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(268, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(389, 136);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(268, 20);
+            this.textBox4.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(273, 55);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Status de l\'application";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(212, 100);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(174, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Temps Authorisé en (jour).hh:mm:ss";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(203, 139);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(183, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Temps de blocage en (jour).hh:mm:ss";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(389, 177);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(268, 20);
+            this.textBox5.TabIndex = 21;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(269, 180);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(114, 13);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "PID du processus actif";
+            // 
+            // radioButtonUnlock
+            // 
+            this.radioButtonUnlock.AutoSize = true;
+            this.radioButtonUnlock.Checked = true;
+            this.radioButtonUnlock.Location = new System.Drawing.Point(18, 26);
+            this.radioButtonUnlock.Name = "radioButtonUnlock";
+            this.radioButtonUnlock.Size = new System.Drawing.Size(73, 17);
+            this.radioButtonUnlock.TabIndex = 23;
+            this.radioButtonUnlock.TabStop = true;
+            this.radioButtonUnlock.Text = "Unlock it !";
+            this.radioButtonUnlock.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonLock
+            // 
+            this.radioButtonLock.AutoSize = true;
+            this.radioButtonLock.Location = new System.Drawing.Point(133, 26);
+            this.radioButtonLock.Name = "radioButtonLock";
+            this.radioButtonLock.Size = new System.Drawing.Size(63, 17);
+            this.radioButtonLock.TabIndex = 24;
+            this.radioButtonLock.Text = "Lock it !";
+            this.radioButtonLock.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonUnlock);
+            this.groupBox1.Controls.Add(this.radioButtonLock);
+            this.groupBox1.Location = new System.Drawing.Point(403, 231);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(245, 53);
+            this.groupBox1.TabIndex = 25;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Status Lock";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.appBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,8 +343,29 @@ namespace WindowsFormsApp1
         private ListBox listBox1;
         private TextBox textBox1;
         private Label label1;
-        private DataGridView dataGridView1;
         private BindingSource appBindingSource;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private NumericUpDown numericUpDown3;
+        private NumericUpDown numericUpDown4;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label8;
+        private Button button1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label7;
+        private Label label9;
+        private Label label10;
+        private TextBox textBox5;
+        private Label label11;
+        private RadioButton radioButtonUnlock;
+        private RadioButton radioButtonLock;
+        private GroupBox groupBox1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -14,6 +14,13 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        private String connectionString = "Data Source=LAPTOP-QNN1SCH4;Initial Catalog=AppSmartLockerdb1;Integrated Security = True";
+        private String currentSelectedProc = "";
+        private const string column_IsLocked = "ISLOCKED";
+        private const string column_TimeAuth = "AUTHTIME";
+        private const string column_TimeLock = "LOCKTIME";
+        private const string column_NameProcess = "NAMEPROCESS";
+        private const int second_by_day = 86400;
         public Form1()
         {
             InitializeComponent();
@@ -23,7 +30,6 @@ namespace WindowsFormsApp1
         {
            
             ////////////////CREATION DE LA LIST DES APPLICATIONS ACTIVES & INSERTION EN BDD
-            String connectionString = "Data Source=LAPTOP-QNN1SCH4;Initial Catalog=AppSmartLockerdb1;Integrated Security = True";
             DataTable dt = new DataTable();
             dt.Columns.Add("ProcessName");
             dt.Columns.Add("ProcessId");
@@ -36,8 +42,8 @@ namespace WindowsFormsApp1
                         dt.Rows.Add();
                         dt.Rows[dt.Rows.Count - 1][0] = p.MainWindowTitle;
                         dt.Rows[dt.Rows.Count - 1][1] = p.Id.ToString();
-                        InsertDataApp(connectionString, p.ProcessName, p.MainWindowTitle, p.Id, p.MainModule.FileName, 0, 24);
-                        Console.WriteLine(p.ProcessName);
+                        InsertDataApp(connectionString, p.ProcessName, p.MainWindowTitle, p.Id, p.MainModule.FileName, 0, second_by_day, 0);
+                        Console.WriteLine("INSERTION REUSSI : " + p.ProcessName);
                     }
                 }
                 catch (Exception err) 
@@ -51,7 +57,6 @@ namespace WindowsFormsApp1
             listBox1.DataSource = dt;
             listBox1.DisplayMember = "ProcessName";
             listBox1.ValueMember = "ProcessId";
-            Console.WriteLine("PUUUUUUUTE3");
 
             /////////////////////////////////////////////////////////
             
@@ -62,11 +67,11 @@ namespace WindowsFormsApp1
         /////////////////////////////////////////LES REQUETES EN BDD///////////////////////////////////////////
         ///////////LES INSERTS///////
         ///
-        private void InsertDataApp(string connectionString, string nameProcess, string libelle, int pid, string chemin, int isLocked, int authTime)
+        private void InsertDataApp(string connectionString, string nameProcess, string libelle, int pid, string chemin, int isLocked, int authTime, int lockTime)
         {
             // define INSERT query with parameters
-            string query = "INSERT INTO dbo.APP (NAMEPROCESS, LIBELLE, PID, CHEMIN, ISLOCKED, AUTHTIME) " +
-                           "VALUES (@nameProcess, @libelle, @pid, @chemin, @isLocked, @authTime) ";
+            string query = "INSERT INTO dbo.APP (NAMEPROCESS, LIBELLE, PID, CHEMIN, ISLOCKED, AUTHTIME, LOCKTIME) " +
+                           "VALUES (@nameProcess, @libelle, @pid, @chemin, @isLocked, @authTime, @lockTime) ";
 
             // create connection and command
             using (SqlConnection cn = new SqlConnection(connectionString))
@@ -79,12 +84,12 @@ namespace WindowsFormsApp1
                 cmd.Parameters.Add("@chemin", SqlDbType.VarChar, 50).Value = chemin;
                 cmd.Parameters.Add("@isLocked", SqlDbType.Int).Value = isLocked;
                 cmd.Parameters.Add("@authTime", SqlDbType.Int).Value = authTime;
+                cmd.Parameters.Add("@lockTime", SqlDbType.Int).Value = lockTime;
                 // open connection, execute INSERT, close connection
                 cn.Open();
                 cmd.ExecuteNonQuery();
                 cn.Close();
             }
-            Console.WriteLine("ET OUAI PUTE");
         }
 
         /////LES UPDATES
@@ -98,24 +103,7 @@ namespace WindowsFormsApp1
                 // define parameters and their values
                 cmd.Parameters.Add("@pid", SqlDbType.Int).Value = pid;
                 cmd.Parameters.Add("@nameProcess", SqlDbType.VarChar, 50).Value = nameProcess;
-                // open connection, execute INSERT, close connection
-                cn.Open();
-                cmd.ExecuteNonQuery();
-                cn.Close();
-            }
-        }
-
-        private void UpdateDataAppTimeAuth(string connectionString, string nameProcess, int timeAuth)
-        {
-            string query = "UPDATE dbo.APP SET TIMEAUTH = @timeAuth WHERE NAMEPROCESS = @nameProcess";
-            // create connection and command
-            using (SqlConnection cn = new SqlConnection(connectionString))
-            using (SqlCommand cmd = new SqlCommand(query, cn))
-            {
-                // define parameters and their values
-                cmd.Parameters.Add("@timeAuth", SqlDbType.Int).Value = timeAuth;
-                cmd.Parameters.Add("@nameProcess", SqlDbType.VarChar, 50).Value = nameProcess;
-                // open connection, execute INSERT, close connection
+                // open connection, execute UPDATE, close connection
                 cn.Open();
                 cmd.ExecuteNonQuery();
                 cn.Close();
@@ -132,33 +120,143 @@ namespace WindowsFormsApp1
                 // define parameters and their values
                 cmd.Parameters.Add("@isLocked", SqlDbType.Int).Value = isLocked;
                 cmd.Parameters.Add("@nameProcess", SqlDbType.VarChar, 50).Value = nameProcess;
-                // open connection, execute INSERT, close connection
+                // open connection, execute UPDATE, close connection
                 cn.Open();
                 cmd.ExecuteNonQuery();
                 cn.Close();
             }
         }
 
+        private void UpdateDataAppOnSaveRestriction(string connectionString, string nameProcess, int isLocked, int authTime, int lockTime)
+        {
+            string query = "UPDATE dbo.APP SET ISLOCKED = @isLocked, AUTHTIME = @authTime, LOCKTIME = @lockTime WHERE NAMEPROCESS = @nameProcess";
+            // create connection and command
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, cn))
+            {
+                // define parameters and their values
+                cmd.Parameters.Add("@nameProcess", SqlDbType.VarChar, 50).Value = nameProcess;
+                cmd.Parameters.Add("@isLocked", SqlDbType.Int).Value = isLocked;
+                cmd.Parameters.Add("@authTime", SqlDbType.Int).Value = authTime;
+                cmd.Parameters.Add("@lockTime", SqlDbType.Int).Value = lockTime;
+                // open connection, execute UPDATE, close connection
+                cn.Open();
+                cmd.ExecuteNonQuery();
+                cn.Close();
+            }
+            Console.WriteLine("Restriction Enregistrée");
+        }
+
+        /////LES SELECTS
+        private int  SelectDataAppColumnInt(string connectionString, string nameProcess, string column)
+        {
+            int result = -1;
+            string query = "SELECT "+column+" FROM dbo.APP WHERE NAMEPROCESS = @nameProcess";
+            // create connection and command
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, cn))
+            {
+                // define parameters and their values
+                cmd.Parameters.Add("@nameProcess", SqlDbType.VarChar, 50).Value = nameProcess;
+                // open connection, execute SELECT, close connection
+                cn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                reader.Read();
+                result = reader.GetInt32(0);
+                cn.Close();
+            }
+            return result;
+        }
+
+
+
         ///////////////////////////////////////FIN DES REQUETES/////////////////////////////////////////////
 
-        private void label1_Click(object sender, EventArgs e)
+        //////////////////////////////////////FONCTIONS UTILES////////////////////////////////////////////
+        private int HeureMinConvertInSeconde(int heure, int min)
         {
+            return heure * 60 * 60 + min * 60;
 
         }
 
+        private String SecondConvertInHeureMinSecond(int second)
+        {
+            return TimeSpan.FromSeconds(second).ToString();
+        }
+
+
+        //////////////////////////////////////LES EVENTS///////////////////////////////////////////////////
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (listBox1.ValueMember != "")
             {
                 int pid = int.Parse(listBox1.SelectedValue.ToString());
                 String procName = Process.GetProcessById(pid).ProcessName;
+                currentSelectedProc = procName;
                 textBox1.Text = procName;
+                textBox5.Text = pid.ToString();
+                
+                if(SelectDataAppColumnInt(connectionString, procName, column_IsLocked) == 0)
+                {
+                    textBox2.Text = "Unlocked process";
+                    radioButtonUnlock.Checked = true;
+                } else if (SelectDataAppColumnInt(connectionString, procName, column_IsLocked) == 1)
+                {
+                    textBox2.Text = "Lock Active on this process, there are a restriction on it !";
+                    radioButtonLock.Checked = true;
+                } else
+                {
+                    textBox2.Text = "Problem on locker status";
+                }
+                
+                textBox3.Text = SecondConvertInHeureMinSecond(SelectDataAppColumnInt(connectionString, procName, column_TimeAuth));
+                textBox4.Text = SecondConvertInHeureMinSecond(SelectDataAppColumnInt(connectionString, procName, column_TimeLock));
+
             }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(currentSelectedProc != "")
+            {
+                String statusWantedstr = "";
+                int statusWanted = -1;
+                int authTimeWanted = -1;
+                int lockTimeWanted = -1;
+                foreach (RadioButton rb in groupBox1.Controls)
+                {
+                    if (rb.Checked == true)
+                    {
+                        statusWantedstr += rb.Name;
+                        if (statusWantedstr == "radioButtonUnlock")
+                        {
+                            statusWanted = 0;
+                        }
+                        else if (statusWantedstr == "radioButtonLock")
+                        {
+                            statusWanted = 1;
+                        }
+                    }
+                }
+                authTimeWanted = HeureMinConvertInSeconde((int)numericUpDown1.Value, (int)numericUpDown3.Value);
+                lockTimeWanted = HeureMinConvertInSeconde((int)numericUpDown2.Value, (int)numericUpDown4.Value);
+                Console.WriteLine(statusWanted);
+                Console.WriteLine(authTimeWanted);
+                Console.WriteLine(lockTimeWanted);
+                UpdateDataAppOnSaveRestriction(connectionString, currentSelectedProc, statusWanted, authTimeWanted, lockTimeWanted);
+                MessageBox.Show("Restriction enregistrée en BDD");
+            } else
+            {
+                MessageBox.Show("Vous devez selectionner un processus pour pouvoir le bloquer !");
+            }
+            
         }
 
         private void bindingSource1_CurrentChanged(object sender, EventArgs e)
         {
 
         }
+
     }
 }
